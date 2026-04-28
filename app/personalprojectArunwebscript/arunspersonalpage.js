3ae6971939aee4ff7f54ad4c8e6d9d318304c8d1
Arun <arun_graeff@stu.indianhills.edu>
2026-04-27 21:58:03 -0500

    descriptionforpersonalproj

--- app/personalprojectArunwebscript/arunspersonalpage.js
+++ app/personalprojectArunwebscript/arunspersonalpage.js
@@ -4,13 +4,14 @@ import { useState } from "react";
  this personal project will be a web profile with an included api
 */
 
-export default function ArunPersonalPage() {}
-const [name, setName] = useState("Arun Graeff");
+export default function ArunPersonalPage() {
+  const [name, setName] = useState("Arun Graeff");
 
-return (
-  <div style={Styles.container}>
-    <h1 style={styles.title}>Arun Graeff</h1>
+  return (
+    <div style={Styles.container}>
+      <h1 style={styles.title}>Arun Graeff</h1>
 
-    <div style={styles.card}></div>
-  </div>
-);
+      <div style={styles.card}></div>
+    </div>
+  );
+}
